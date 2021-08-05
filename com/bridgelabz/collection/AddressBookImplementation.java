2b16e9d77aa9dc02056fd59954198dc784dba507
manjunathhalli <manjunathhalli629@gmail.com>
2021-08-06 01:04:00 +0530

    create address book

--- com/bridgelabz/collection/AddressBookImplementation.java
+++ com/bridgelabz/collection/AddressBookImplementation.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.collection;
 
 public class AddressBookImplementation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Problem");
+    }
+
 }
---
+++ com/bridgelabz/collection/IAddressBook.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.collection;
+
+public interface IAddressBook {
+}
